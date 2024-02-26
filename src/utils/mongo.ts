d30ceae9f40773f90bf248387afcf0739ba08b21
Eissa Saber <eissapk44@gmail.com>
2024-02-26 12:30:30 +0200

    added log

--- src/utils/mongo.ts
+++ src/utils/mongo.ts
@@ -4,6 +4,7 @@ const { MONGO_URI = "" } = process.env;
 export const connectDB = async () => {
 	try {
 		await mongoose.connect(MONGO_URI);
+		console.log("Connected to DB.");
 	} catch (err) {
 		console.error(err);
 		process.exit(1);
