f572b42db5d4ca2993729bf3b5c406f38459fd70
Eissa Saber <eissapk44@gmail.com>
2024-03-27 03:17:08 +0200

    fixed errors in editorTools file

--- src/utils/editorTools.tsx
+++ src/utils/editorTools.tsx
@@ -1,17 +1,32 @@
+// @ts-expect-error
 import Embed from "@editorjs/embed";
+// @ts-expect-error
 import Table from "@editorjs/table";
+// @ts-expect-error
 import List from "@editorjs/list";
+// @ts-expect-error
 import Warning from "@editorjs/warning";
+// @ts-expect-error
 import Code from "@editorjs/code";
+// @ts-expect-error
 import LinkTool from "@editorjs/link";
+// @ts-expect-error
 import Image from "@editorjs/image";
+// @ts-expect-error
 import Raw from "@editorjs/raw";
+// @ts-expect-error
 import Header from "@editorjs/header";
+// @ts-expect-error
 import Quote from "@editorjs/quote";
+// @ts-expect-error
 import Marker from "@editorjs/marker";
+// @ts-expect-error
 import CheckList from "@editorjs/checklist";
+// @ts-expect-error
 import Delimiter from "@editorjs/delimiter";
+// @ts-expect-error
 import InlineCode from "@editorjs/inline-code";
+// @ts-expect-error
 import SimpleImage from "@editorjs/simple-image";
 
 export const EDITOR_JS_TOOLS = {
